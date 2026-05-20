695ca47dfa5e05461a69566247db7e4bdc51fff1
huchenlei <huchenlei@cyberlink.ai>
2026-05-20 13:08:14 +0800

    静态资源加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 
 import ArticleView from '@/views/ArticleView.vue'
 import CategoryView from '@/views/CategoryView.vue'
@@ -38,7 +38,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return { top: 0, behavior: 'smooth' }
